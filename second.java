76b691d6817a857143f04b693bed7998389a6e36
vengatesh116 <30826261+vengatesh116@users.noreply.github.com>
2017-08-28 09:50:31 +0530

    Add files via upload

--- second.java
+++ second.java
@@ -1,41 +1,19 @@
-import java.util.*;
-public class minimum 
-{
-public static void main(String ar[])throws Exception
-{
-    int r,i,k;
-    int p[]=new int[100];
-Scanner sc=new Scanner(System.in);
-minimum m1=new minimum();
-String a=new String();
-a=sc.next();
-k=sc.nextInt();
-int n=Integer.parseInt(a);
-i=0;
-while(n>0)
-{
-r=n%10;
-p[i++]=r;
-n/=10;
-}
-m1.sort(p,k,i);
-}
-public void sort(int l[],int m,int g)
-{
-    int t;
-for(int i=0;i<g-1;i++)
-{
-for(int j=i+1;j<g;j++)
-{
-if(l[i]>l[j])
-{
-t=l[i];
-l[i]=l[j];
-l[j]=t;
-}
-}
-}
-for(int i=0;i<m;i++)
-    System.out.print(l[i]);
-}
-}
+# five
+import java.awt.*;
+ import acm.program.*;
+ import acm.graphics.*;
+  public class Tree extends GraphicsProgram 
+  public void run() {
+    drawTree(120, 200, 50, 90);
+    }
+  
+  public void drawTree(double x0, double y0, double len, double angle) {
+  if(len > 2) {
+   double x1 = x0 + len * GMath.cosDegrees(angle);
+   double y1 = y0 - len * GMath.sinDegrees(angle);
+ add(new GLine(x0, y0, x1, y1));
+ drawTree(x1, y1, len * 0.75, angle + 30);
+  drawTree(x1, y1, len * 0.66, angle - 50);
+    }
+  }
+  }
