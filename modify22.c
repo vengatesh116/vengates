29b7c92f81828834e30e0557c9a3e05923ab6361
vengatesh116 <30826261+vengatesh116@users.noreply.github.com>
2017-08-28 10:19:00 +0530

    Add files via upload

---
+++ modify22.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+void main()
+{
+int a1,a2,a3,a4,y1,y2,y3,y4,c1,c2,c3,c4,b1.b2,b3,b4;
+clrscr();
+scanf("%d,%d",x1,y1);
+printf("(%d,%d)",x1,y1);
+a2=a1;
+scanf("%d",&y2);
+printf("(%d,%d)",x2,y2);
+y3=y1;
+scanf("%d",&x3);
+printf("(%d,%d)",x3,y3);
+a4=a3;
+y4=y2;
+printf("(%d,%d)",x4,y4);
+scanf("%d,%d",a1,b1);
+printf("(%d,%d)",a1,b1);
+c2=c1;
+scanf("%d",&b2);
+printf("(%d,%d)",a2,b2);
+b3=b1;
+scanf("%d",&a3);
+printf("(%d,%d)",a3,b3);
+c4=c3;
+b4=b2;
+printf("(%d,%d)",a4,b4);
+if((x1<=a1&&x2<=b2)&&(x3<=b3&&x4<=b4))
+{
+printf("F");
+}
+else
+{
+printf("T");
+}
+getch();
+}
