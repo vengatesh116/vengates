69989b96928a10855b47854ff1034e954f84da6f
vengatesh116 <30826261+vengatesh116@users.noreply.github.com>
2017-08-28 10:21:25 +0530

    Add files via upload

---
+++ modify23.c
@@ -0,0 +1,22 @@
+include<stdio.h>
+int main()
+{
+int i,m,x,y,c,d;
+printf("A robot have 2 type of commands\n");
+scanf("%d",&m);
+for(i=0;i<=x;i++)
+{
+if(x>=0 && x<=10000)
+{
+ printf("com1: forward%d",x);
+}
+}
+for(i=0;i<=y;i++)
+{
+if(y==-180 && y==180)
+{
+printf("com2: X degree:%d",y);
+}
+}
+return 0;
+}
